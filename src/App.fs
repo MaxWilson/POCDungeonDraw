15a9b09108d33d618df4550e8b6cfd6b20132481
Max Wilson <wilson.max@gmail.com>
2022-10-08 12:55:01 -0700

    Rudimentary notification support

--- src/App.fs
+++ src/App.fs
@@ -29,6 +29,8 @@ module SketchCanvas =
         static member inline strokeColor (v: string) = mkSketchAttr "strokeColor" v
         static member inline style (props: IStyleAttribute list) : ISketchProperty = !!Interop.mkAttr "style" (createObj !!props)
         static member inline ref (ref: IRefValue<#Browser.Types.HTMLElement option>): ISketchProperty = !!Interop.mkAttr "ref" ref
+        static member inline onStroke (handler: obj -> unit): ISketchProperty = mkSketchAttr "onStroke" handler
+        static member inline onChange (handler: obj -> unit): ISketchProperty = mkSketchAttr "onChange" handler
     [<Erase>]
     type style =
         static member inline border (v:string) = Interop.mkStyle "border" v
@@ -70,23 +72,29 @@ open SketchCanvas
 let sketchpad dispatch =
     let canvas = React.useRef(None)
     let html, htmlUpdate = React.useState "Press the Export SVG button"
+    let lastPath, lastPathUpdate = React.useState "No paths yet"
+    let lastStroke, lastStrokeUpdate = React.useState "No strokes yet"
     Html.div [
+        let exportSvg _ =
+            promise {
+                match canvas.current with
+                | Some canvas ->
+                    let! (svg: string) = canvas?exportSvg()
+                    htmlUpdate svg
+                | None -> ()
+            } |> Promise.start
         sketch.create [
             sketch.ref canvas
             sketch.style [style.border "1em dashed purple"]
             sketch.height 400; sketch.width 600; sketch.strokeWidth 4; sketch.strokeColor "blue"
+            //sketch.onChange (fun path -> System.Console.WriteLine path)
+            sketch.onStroke (fun path -> System.Console.WriteLine path)
             ]
         Html.div [
-            let exportSvg _ =
-                promise {
-                    match canvas.current with
-                    | Some canvas ->
-                        let! (svg: string) = canvas?exportSvg()
-                        htmlUpdate svg
-                    | None -> ()
-                } |> Promise.start
             Html.button [prop.text "Export SVG"; prop.onClick(exportSvg)]
             Html.textarea [prop.value html; prop.readOnly true]
+            Html.textarea [prop.value lastStroke; prop.readOnly true]
+            Html.textarea [prop.value lastPath; prop.readOnly true]
             ]
         ]
 
