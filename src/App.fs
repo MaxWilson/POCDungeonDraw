54ce6b2039e038001e3a2d1ca75034b37f26d7f5
Max Wilson <wilson.max@gmail.com>
2022-10-08 21:34:50 -0700

    Refactor slightly

--- src/App.fs
+++ src/App.fs
@@ -8,33 +8,6 @@ open Fable.Core.JsInterop
 
 type 't Deferred = NotStarted | InProgress | Ready of 't
 
-module SketchCanvas =
-    open Feliz
-    open Fable.Core
-    open Fable.Core.JsInterop
-
-    [<Erase>]
-    type ISketchProperty =
-        interface end
-
-    [<AutoOpen>]
-    module private Interop =
-        let inline mkSketchAttr (key: string) (value: obj) : ISketchProperty = unbox (key, value)
-    [<Erase>]
-    type sketch =
-        static member inline create (props: ISketchProperty list) = Interop.reactApi.createElement(import "ReactSketchCanvas" "react-sketch-canvas", createObj !!props)
-        static member inline width (v:int) = mkSketchAttr "width" v
-        static member inline height (v: int) = mkSketchAttr "height" v
-        static member inline strokeWidth (v: int) = mkSketchAttr "strokeWidth" v
-        static member inline strokeColor (v: string) = mkSketchAttr "strokeColor" v
-        static member inline style (props: IStyleAttribute list) : ISketchProperty = !!Interop.mkAttr "style" (createObj !!props)
-        static member inline ref (ref: IRefValue<#Browser.Types.HTMLElement option>): ISketchProperty = !!Interop.mkAttr "ref" ref
-        static member inline onStroke (handler: obj -> unit): ISketchProperty = mkSketchAttr "onStroke" handler
-        static member inline onChange (handler: obj -> unit): ISketchProperty = mkSketchAttr "onChange" handler
-    [<Erase>]
-    type style =
-        static member inline border (v:string) = Interop.mkStyle "border" v
-        static member inline borderRadius (v:string) = Interop.mkStyle "borderRadius" v
 
 [<ReactComponent>]
 let Counter () =
@@ -67,7 +40,8 @@ let Message () =
                 | InProgress -> Html.div "Executing..."
                 | Ready (msg:string) -> Html.p msg
             ]
-open SketchCanvas
+
+open Components.SketchCanvas
 [<ReactComponent>]
 let sketchpad dispatch =
     let canvas = React.useRef(None)
@@ -87,7 +61,7 @@ let sketchpad dispatch =
             sketch.ref canvas
             sketch.style [style.border "0.06em dashed purple"]
             sketch.height 400; sketch.width 600; sketch.strokeWidth 4; sketch.strokeColor "blue"
-            //sketch.onChange (fun path -> System.Console.WriteLine path)
+            sketch.onChange (fun path -> System.Console.WriteLine path)
             sketch.onStroke (fun path -> System.Console.WriteLine path)
             ]
         Html.div [
---
+++ src/Components.fs
@@ -0,0 +1,28 @@
+﻿module Components
+    module SketchCanvas =
+        open Feliz
+        open Fable.Core
+        open Fable.Core.JsInterop
+
+        [<Erase>]
+        type ISketchProperty =
+            interface end
+
+        [<AutoOpen>]
+        module private Interop =
+            let inline mkSketchAttr (key: string) (value: obj) : ISketchProperty = unbox (key, value)
+        [<Erase>]
+        type sketch =
+            static member inline create (props: ISketchProperty list) = Interop.reactApi.createElement(import "ReactSketchCanvas" "react-sketch-canvas", createObj !!props)
+            static member inline width (v:int) = mkSketchAttr "width" v
+            static member inline height (v: int) = mkSketchAttr "height" v
+            static member inline strokeWidth (v: int) = mkSketchAttr "strokeWidth" v
+            static member inline strokeColor (v: string) = mkSketchAttr "strokeColor" v
+            static member inline style (props: IStyleAttribute list) : ISketchProperty = !!Interop.mkAttr "style" (createObj !!props)
+            static member inline ref (ref: IRefValue<#Browser.Types.HTMLElement option>): ISketchProperty = !!Interop.mkAttr "ref" ref
+            static member inline onStroke (handler: obj -> unit): ISketchProperty = mkSketchAttr "onStroke" handler
+            static member inline onChange (handler: obj -> unit): ISketchProperty = mkSketchAttr "onChange" handler
+        [<Erase>]
+        type style =
+            static member inline border (v:string) = Interop.mkStyle "border" v
+            static member inline borderRadius (v:string) = Interop.mkStyle "borderRadius" v
