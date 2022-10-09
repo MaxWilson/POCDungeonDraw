4223b4f0f0b3f581deefe9bd983294f479c066a0
Max Wilson <wilson.max@gmail.com>
2022-10-08 21:55:02 -0700

    Switch to Elmish model instead of raw React

--- src/App.fs
+++ src/App.fs
@@ -1,84 +1,35 @@
 module App
 
 open Browser.Dom
-open Fetch
-open Thoth.Fetch
+
 open Feliz
 open Fable.Core.JsInterop
 
-type 't Deferred = NotStarted | InProgress | Ready of 't
 
+open Components
 
-[<ReactComponent>]
-let Counter () =
-    let (count, setCount) = React.useState (0)
+type Model = { tag: string }
+type Msg = SetTag of string
+let init _ = { tag = System.Guid.NewGuid().ToString() }
 
-    Html.div [ Html.h1 count
-               Html.button [ prop.text "Increment"
-                             prop.onClick (fun _ -> setCount (count + 1)) ] ]
+let update msg model =
+    match msg with
+    | SetTag tag -> { model with tag = tag }
 
-[<ReactComponent>]
-let Message () =
-    let (message, setMessage) = React.useState (NotStarted)
-    Html.div [  Html.button [   prop.text "Get a message from the API"
-                                prop.onClick
-                                    (fun _ ->
-                                        promise {
-                                            setMessage InProgress
-                                            let! message =
-                                                Fetch.get (
-                                                    "/api/GetMessage?name=FSharpie",
-                                                    headers = [ HttpRequestHeaders.Accept "application/json" ]
-                                                )
-
-                                            setMessage (message |> Ready)
-                                            return ()
-                                        }
-                                        |> ignore) ]
-                match message with
-                | NotStarted -> Html.none
-                | InProgress -> Html.div "Executing..."
-                | Ready (msg:string) -> Html.p msg
-            ]
-
-open Components.SketchCanvas
-[<ReactComponent>]
-let sketchpad dispatch =
-    let canvas = React.useRef(None)
-    let html, htmlUpdate = React.useState "Press the Export SVG button"
-    let lastPath, lastPathUpdate = React.useState "No paths yet"
-    let lastStroke, lastStrokeUpdate = React.useState "No strokes yet"
-    Html.div [
-        let exportSvg _ =
-            promise {
-                match canvas.current with
-                | Some canvas ->
-                    let! (svg: string) = canvas?exportSvg()
-                    htmlUpdate svg
-                | None -> ()
-            } |> Promise.start
-        sketch.create [
-            sketch.ref canvas
-            sketch.style [style.border "0.06em dashed purple"]
-            sketch.height 400; sketch.width 600; sketch.strokeWidth 4; sketch.strokeColor "blue"
-            sketch.onChange (fun path -> System.Console.WriteLine path)
-            sketch.onStroke (fun path -> System.Console.WriteLine path)
-            ]
-        Html.div [
-            Html.button [prop.text "Export SVG"; prop.onClick(exportSvg)]
-            Html.textarea [prop.value html; prop.readOnly true]
-            Html.textarea [prop.value lastStroke; prop.readOnly true]
-            Html.textarea [prop.value lastPath; prop.readOnly true]
-            ]
-        ]
-
-[<ReactComponent>]
-let App () =
-    React.fragment [
-        sketchpad()
-        Counter()
-        Message() ]
+let view (model:Model) dispatch =
+    Html.div [  sketchpad()
+                Html.div [
+                    Html.span "Tag:"
+                    Html.input [prop.placeholder "Enter a tag"; prop.valueOrDefault model.tag; prop.onChange (SetTag >> dispatch)]
+                    ]
+                Counter()
+                Message model.tag
+                ]
 
 open Browser.Dom
+open Elmish
+open Elmish.React
 
-ReactDOM.render (App(), document.getElementById "root")
+Program.mkSimple init update view
+    |> Program.withReactBatched "root"
+    |> Program.run
--- src/Components.fs
+++ src/Components.fs
@@ -1,4 +1,10 @@
 ﻿module Components
+open Feliz
+open Fetch
+open Thoth.Fetch
+
+type 't Deferred = NotStarted | InProgress | Ready of 't
+
     module SketchCanvas =
         open Feliz
         open Fable.Core
@@ -26,3 +32,68 @@
         type style =
             static member inline border (v:string) = Interop.mkStyle "border" v
             static member inline borderRadius (v:string) = Interop.mkStyle "borderRadius" v
+open SketchCanvas
+open Fable.Core.JsInterop
+
+[<ReactComponent>]
+let sketchpad dispatch =
+    let canvas = React.useRef(None)
+    let html, htmlUpdate = React.useState "Press the Export SVG button"
+    let lastPath, lastPathUpdate = React.useState "No paths yet"
+    let lastStroke, lastStrokeUpdate = React.useState "No strokes yet"
+    Html.div [
+        let exportSvg _ =
+            promise {
+                match canvas.current with
+                | Some canvas ->
+                    let! (svg: string) = canvas?exportSvg()
+                    htmlUpdate svg
+                | None -> ()
+            } |> Promise.start
+        sketch.create [
+            sketch.ref canvas
+            sketch.style [style.border "0.06em dashed purple"]
+            sketch.height 400; sketch.width 600; sketch.strokeWidth 4; sketch.strokeColor "blue"
+            sketch.onChange (fun path -> System.Console.WriteLine path)
+            sketch.onStroke (fun path -> System.Console.WriteLine path)
+            ]
+        Html.div [
+            Html.button [prop.text "Export SVG"; prop.onClick(exportSvg)]
+            Html.textarea [prop.value html; prop.readOnly true]
+            Html.textarea [prop.value lastStroke; prop.readOnly true]
+            Html.textarea [prop.value lastPath; prop.readOnly true]
+            ]
+        ]
+
+
+[<ReactComponent>]
+let Counter () =
+    let (count, setCount) = React.useState (0)
+
+    Html.div [ Html.h1 count
+               Html.button [ prop.text "Increment"
+                             prop.onClick (fun _ -> setCount (count + 1)) ] ]
+
+[<ReactComponent>]
+let Message (tag:string) =
+    let (message, setMessage) = React.useState (NotStarted)
+    Html.div [  Html.button [   prop.text "Get a message from the API"
+                                prop.onClick
+                                    (fun _ ->
+                                        promise {
+                                            setMessage InProgress
+                                            let! message =
+                                                Fetch.get (
+                                                    $"/api/GetMessage?name={tag}",
+                                                    headers = [ HttpRequestHeaders.Accept "application/json" ]
+                                                )
+
+                                            setMessage (message |> Ready)
+                                            return ()
+                                        }
+                                        |> ignore) ]
+                match message with
+                | NotStarted -> Html.none
+                | InProgress -> Html.div "Executing..."
+                | Ready (msg:string) -> Html.p msg
+            ]
