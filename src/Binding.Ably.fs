7fad490715c96e32d2291e6867548ce6f2f7f3af
Max Wilson <maxw@microsoft.com>
2022-10-11 15:26:24 -0700

    Add ably binding (using it will come later)

---
+++ src/Binding.Ably.fs
@@ -0,0 +1,20 @@
+﻿module Binding.Ably
+
+  type IMessage =
+    abstract name: string
+    abstract data: string
+
+  type IChannel =
+    abstract publish: string * string -> unit
+    abstract subscribe: string * (IMessage -> unit) -> unit
+
+  type IChannels =
+    abstract ``get``: string -> IChannel
+
+  type IRealTime =
+    abstract channels: IChannels
+
+  type IAbly =
+    abstract Realtime: (string) -> IRealTime
+
+  let Ably: IAbly = importAll "Ably"
