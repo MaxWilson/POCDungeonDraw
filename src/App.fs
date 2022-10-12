d261bf72f30067048d17fcca46f36e7f859ab291
Max Wilson <maxw@microsoft.com>
2022-10-12 10:36:42 -0700

    Refactor login state to be local to react (using hooks) instead of global

--- src/App.fs
+++ src/App.fs
@@ -11,45 +11,44 @@ open Elmish
 type 't Deferred = NotStarted | InProgress | Ready of 't
 type IdentityProvider = Facebook | AAD | Erroneous
 type Identity = (IdentityProvider * string) option
-type UIState = Normal | LoggingIn
-type Model = { mode: UIState; tag: string; currentUser: Identity Deferred }
-type Msg = SetTag of string | Login of IdentityProvider option | Logout | ReceiveIdentity of Identity Deferred
-let init _ = { mode = Normal; tag = System.Guid.NewGuid().ToString(); currentUser = NotStarted }, []
+type Model = { tag: string; currentUser: Identity Deferred }
+type Msg = SetTag of string | ReceiveIdentity of Identity Deferred
+let init _ = { tag = System.Guid.NewGuid().ToString(); currentUser = NotStarted }, []
 
 let navigateTo (url: string) =
     Browser.Dom.window.location.assign url
+
+[<ReactComponent>]
+let loginButton dispatch =
+    let loggingIn, update = React.useState (thunk false)
+    if loggingIn then
+        Html.span [
+            Html.text "Log in with"
+            Html.button [prop.text "Facebook"; prop.onClick (thunk1 navigateTo @".auth/login/facebook")]
+            Html.button [prop.text "AAD"; prop.onClick (thunk1 navigateTo @".auth/login/aad")]
+            ]
+    else
+        Html.button [prop.text "Login"; prop.onClick (fun _ -> update true)]
 
 let update msg model =
     match msg with
     | SetTag tag -> { model with tag = tag }, []
     | ReceiveIdentity id -> { model with currentUser = id }, []
-    | Login (Some Facebook) -> { model with mode = Normal }, Cmd.ofSub(fun _ -> navigateTo @".auth/login/facebook")
-    | Login (Some AAD) -> { model with mode = Normal }, Cmd.ofSub(fun _ -> navigateTo @".auth/login/aad")
-    | Login _ -> { model with mode = LoggingIn }, []
-    | Logout -> model, Cmd.ofSub(fun _ -> navigateTo @".auth/logout")
 
 let view (model:Model) dispatch =
     Html.div [
-        match model.mode with
-        | LoggingIn ->
-            Html.div [
-                Html.text "Log in with"
-                Html.button [prop.text "Facebook"; prop.onClick (thunk1 dispatch (Login (Some Facebook)))]
-                Html.button [prop.text "AAD"; prop.onClick (thunk1 dispatch (Login (Some AAD)))]
-                ]
-        | Normal ->
-            match model.currentUser with
-            | NotStarted -> Html.div [prop.text "Initializing identity..."]
-            | InProgress -> Html.div [prop.text "Retreiving identity..."]
-            | Ready None -> Html.div [Html.text "Hello, stranger."; Html.button [prop.text "Login"; prop.onClick (fun _ -> dispatch (Login None))]]
-            | Ready (Some ((Facebook | AAD | Erroneous), accountName)) -> Html.div [Html.text $"Hello, {accountName}"; Html.button [prop.text "Log out"; prop.onClick (fun _ -> dispatch (Logout))]]
-            sketchpad()
-            Html.div [
-                Html.span "Tag:"
-                Html.input [prop.placeholder "Enter a tag"; prop.valueOrDefault model.tag; prop.onChange (SetTag >> dispatch)]
-                ]
-            Counter()
-            Message model.tag
+        match model.currentUser with
+        | NotStarted -> Html.div [prop.text "Initializing identity..."]
+        | InProgress -> Html.div [prop.text "Retreiving identity..."]
+        | Ready None -> Html.div [Html.text "Hello, stranger."; loginButton dispatch]
+        | Ready (Some ((Facebook | AAD | Erroneous), accountName)) -> Html.div [Html.text $"Hello, {accountName}"; Html.button [prop.text "Log out"; prop.onClick (thunk1 navigateTo @".auth/logout")]]
+        sketchpad()
+        Html.div [
+            Html.span "Tag:"
+            Html.input [prop.placeholder "Enter a tag"; prop.valueOrDefault model.tag; prop.onChange (SetTag >> dispatch)]
+            ]
+        Counter()
+        Message model.tag
         ]
 
 open Browser.Dom
