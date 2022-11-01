7f88df9cb171b34ea4f3624a6a43a9bf456fbb19
Max Wilson <wilson.max@gmail.com>
2022-11-01 08:04:11 -0700

    wip

--- src/Lib/Autopicker.fs
+++ src/Lib/Autopicker.fs
@@ -1,6 +1,8 @@
 ﻿module Lib.Autopicker
 
-// create -> acc -> domainType option
+// in order of actual runtime order the values go acc => intermediate => domainType
+// therefore the type is create -> acc -> domainType option
+// and the runtime order of the stages is choice => create
 type ComposedChoice<'acc, 'intermediateState, 'domainType> = ('intermediateState -> 'domainType option) -> 'acc -> 'domainType option
 
 module Choice =
@@ -22,22 +24,23 @@ type Compose() =
         let chooseOne (choices : _ list) create acc =
             choices |> chooseRandom |> pickOne create
         chooseOne options create (acc |> Choice.Param.appendKey label)
-    member _.ctor2 (ctor: _ -> 'ctor) (choice1: ComposedChoice<'acc,'arg1,_>) (choice2 : ComposedChoice<'acc,'arg2,_>) : ComposedChoice<'acc,'ctor,'r> = fun create acc ->
+    member _.ctor2 (ctor: _ -> 'domainType) (choice1: ComposedChoice<'acc,'arg1,_>) (choice2 : ComposedChoice<'acc,'arg2,_>) : ComposedChoice<'acc,_,'domainType> = fun create acc ->
         if rand.Next 10 < 3 then
             ctor(1,2) |> (pickOne create)
         elif rand.Next 10 < 3 then
-            let choicex = choice1
-            let choice1 = notImpl()
-            choice1 (Option.bind (fun arg1 -> ctor(1, 2) |> pickOne create))
+            choice1 (Option.map (fun arg1 -> ctor(1, 2))) acc
+        elif rand.Next 10 < 3 then
+            choice1 (Option.bind (fun arg1 -> ctor(1, 2) |> pickOne create)) acc
         else
-            let create2 arg1 arg2 =
-                match arg2 with
-                | Some arg2 -> ctor(arg1, arg2) |> pickOne create
-                | None -> None
-            let create1 = function
-                | Some arg1 -> choice2 (function arg2 -> create2 arg1 arg2) acc
-                | None -> None
-            choice1 create1
+            //let create2 arg1 arg2 =
+            //    match arg2 with
+            //    | Some arg2 -> ctor(arg1, arg2) |> pickOne create
+            //    | None -> None
+            //let create1 = function
+            //    | Some arg1 -> choice2 (function arg2 -> create2 arg1 arg2) acc
+            //    | None -> None
+            //choice1 (Option.bind (fun arg1 -> ctor(1, 2) |> pickOne create)) acc
+            notImpl()
 
-    member _.ctor choice ctor : ComposedChoice<_,_,_> = fun create -> choice (Option.map ctor >> k)
+    member _.ctor choice ctor : ComposedChoice<_,_,_> = fun create -> choice (Option.map (ctor >> create))
 let compose = Compose()
