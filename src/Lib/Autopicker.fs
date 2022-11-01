913d697daee9d54b575ea4555671868370fef71b
Max Wilson <wilson.max@gmail.com>
2022-11-01 07:44:13 -0700

    wip

--- src/Lib/Autopicker.fs
+++ src/Lib/Autopicker.fs
@@ -1,6 +1,6 @@
 ﻿module Lib.Autopicker
 
-type ComposedChoice<'acc, 'intermediateState, 'r> = ('intermediateState -> 'r) -> 'acc -> 'r
+type ComposedChoice<'acc, 'intermediateState, 'domainType> = ('intermediateState -> 'domainType option) -> 'acc -> 'domainType option
 module Choice =
     type Param = { probabilityInPercent: int; key: string list }
         with
@@ -8,32 +8,35 @@ module Choice =
         static member appendKey newValue old = { old with key = old.key @ [newValue] }
 
 type Compose() =
-    let pickOne k item =
-        Some item |> k
-    let pickSome k (items: _ list) =
-        Some items |> k
-    member _.from label (suboptions: ComposedChoice<_,_,_> list) : ComposedChoice<_,_,_> = fun gen acc ->
+    let pickOne create item =
+        item |> Option.bind create
+    let pickSome create (items: _ list) =
+        items |> List.map (Option.bind create)
+    member _.from label (suboptions: ComposedChoice<_,_,_> list) : ComposedChoice<_,_,_> = fun create acc ->
         let acc = acc |> Choice.Param.appendKey label
-        (chooseRandom suboptions) |> (pickOne gen)
-    member _.a v : ComposedChoice<_,_,_> = fun gen acc -> gen [v]
-    member _.oneOf label options : ComposedChoice<_,_,_> = fun gen acc ->
-        let chooseOne (choices : _ list) gen acc =
-            choices |> chooseRandom |> pickOne gen
-        chooseOne options gen (acc |> Choice.Param.appendKey label)
-    member _.ctor2 (ctor: _ -> 'ctor) (choice1: ComposedChoice<'acc,'arg1,_>) (choice2 : ComposedChoice<'acc,'arg2,_>) : ComposedChoice<'acc,'ctor option,'r> = fun gen acc ->
+        (chooseRandom suboptions) |> Some |> (pickOne create)
+    member _.a v : ComposedChoice<_,_,_> = fun create acc -> pickOne create v
+    member _.oneOf label options : ComposedChoice<_,_,_> = fun create acc ->
+        let chooseOne (choices : _ list) create acc =
+            choices |> chooseRandom |> pickOne create
+        chooseOne options create (acc |> Choice.Param.appendKey label)
+    member _.ctor2 (ctor: _ -> 'ctor) (choice1: ComposedChoice<'acc,'arg1,_>) (choice2 : ComposedChoice<'acc,'arg2,_>) : ComposedChoice<'acc,'ctor,'r> = fun create acc ->
         if rand.Next 10 < 3 then
-            ctor(1,2) |> (pickOne gen)
+            ctor(1,2) |> Some |> (pickOne create)
         elif rand.Next 10 < 3 then
-            choice1 (fun arg1 -> ctor(1, 2) |> pickOne gen)
+            let choicex = choice1
+            let choice1 = notImpl()
+            choice1 (Option.bind (fun arg1 -> ctor(1, 2) |> Some |> pickOne create))
         else
-            let gen2 arg1 arg2 =
-                match arg2 with
-                | Some arg2 -> ctor(arg1, arg2) |> pickOne gen
-                | None -> None |> gen
-            let gen1 = function
-                | Some arg1 -> choice2 (function arg2 -> gen2 arg1 arg2)
-                | None -> fun acc -> None |> gen
-            choice1 gen1
+            notImpl()
+            //let create2 arg1 arg2 =
+            //    match arg2 with
+            //    | Some arg2 -> ctor(arg1, arg2) |> pickOne create
+            //    | None -> None |> create
+            //let create1 = function
+            //    | Some arg1 -> choice2 (function arg2 -> create2 arg1 arg2)
+            //    | None -> fun acc -> None |> create
+            //choice1 create1
 
-    member _.ctor choice ctor : ComposedChoice<_,_,_> = fun gen -> choice (Option.map ctor >> k)
+    member _.ctor choice ctor : ComposedChoice<_,_,_> = fun create -> choice (Option.map ctor >> k)
 let compose = Compose()
