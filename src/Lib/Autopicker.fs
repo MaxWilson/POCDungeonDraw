955ab9e10deb47ee38bcdcc70448a60c96c29cbb
Max Wilson <wilson.max@gmail.com>
2022-11-01 08:09:06 -0700

    wip

--- src/Lib/Autopicker.fs
+++ src/Lib/Autopicker.fs
@@ -25,21 +25,7 @@ type Compose() =
             choices |> chooseRandom |> pickOne create
         chooseOne options create (acc |> Choice.Param.appendKey label)
     member _.ctor2 (ctor: _ -> 'constructedType) (choice1: ComposedChoice<'acc,'arg1,_>) (choice2 : ComposedChoice<'acc,'arg2,_>) : ComposedChoice<'acc,'constructedType,'domainType> = fun create acc ->
-        if rand.Next 10 < 3 then
-            ctor(1,2) |> (pickOne create)
-        elif rand.Next 10 < 3 then
-            choice1 (Option.map (fun arg1 -> ctor(1, 2))) acc
-        elif rand.Next 10 < 3 then
-            choice1 (Option.bind (fun arg1 -> ctor(1, 2) |> pickOne create)) acc
-        else
-            let create2 arg1 arg2 =
-                match arg2 with
-                | Some arg2 -> ctor(arg1, arg2) |> pickOne create
-                | None -> None
-            let create1 = function
-                | Some arg1 -> choice2 (function arg2 -> create2 arg1 arg2) acc
-                | None -> None
-            choice1 (Option.bind (fun arg1 -> ctor(1, 2) |> pickOne create)) acc
+        choice1 (Option.bind (fun arg1 -> ctor(1, 2) |> pickOne create)) acc
 
     member _.ctor choice ctor : ComposedChoice<_,_,_> = fun create -> choice (Option.map (ctor >> create))
 let compose = Compose()
