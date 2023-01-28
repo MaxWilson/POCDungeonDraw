945d5b918e5aa55fb8f1131abcc9d38842744653
Max Wilson <wilson.max@gmail.com>
2023-01-28 12:04:04 -0800

    Workaround for wait-on issue: https://github.com/jeffbski/wait-on/issues/109

--- startAll.cmd
+++ startAll.cmd
@@ -1,5 +1,5 @@
 pushd api && start func start --csharp
 popd
 start npm start
-start swa start http://localhost:3000 --api-location http://localhost:7071
+start swa start http://127.0.0.1:3000 --api-location http://127.0.0.1:7071
 start http://localhost:4280/
