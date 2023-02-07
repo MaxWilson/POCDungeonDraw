7bd1483e23c27230fd244c1aa7a31fd51692ff45
Max Wilson <wilson.max@gmail.com>
2023-02-07 06:25:37 -0800

    wip on JavaScript-side pubsub

---
+++ src/WebSync.fs
@@ -0,0 +1,90 @@
+﻿module WebSync
+
+open Fable.Core
+open Browser.Types
+
+let socket: WebSocket option = None
+
+let tryConnect(clientUrl) =
+    let isConnected =
+        match socket with
+        | None -> false
+        | Some socket -> socket.readyState = WebSocketState.OPEN
+    ()
+
+(* TEMPLATE JavaScript (imperative)
+  const isConnected = webSocket?.readyState === WebSocket.OPEN;
+  if (!isConnected) {
+    let tokenResponse = await fetch(
+      `/api/CreateTokenRequest/${hubName}/${groupName}/${user.id}`
+    );
+    let clientUrl = await tokenResponse.text();
+    webSocket = new WebSocket(clientUrl, "json.webpubsub.azure.v1");
+    webSocket.onopen = () => {
+      console.log("Connected 🎉");
+      select("#connectButton").elt.innerText = "Disconnect";
+      webSocket.send(
+        JSON.stringify({
+          type: "joinGroup",
+          group: groupName,
+        })
+      );
+      addUser(user.id, user.strokeColor);
+      webSocket.send(
+        JSON.stringify({
+          type: "sendToGroup",
+          group: groupName,
+          noEcho: true,
+          data: {
+            messageType: joinedMessage,
+            clientId: user.id,
+            color: user.strokeColor,
+          }
+        })
+      );
+    };
+    webSocket.onclose = (event) => {
+      console.log(`Disconnected 😿, code=${event.code}`);
+      select("#connectButton").elt.innerText = "Connect";
+    };
+    webSocket.onmessage = (event) => {
+      const message = JSON.parse(event.data);
+      if (message.type === "message") {
+        switch (message.data.messageType) {
+          case hoverPositionMessage:
+            setUserPosition(
+              message.data.clientId,
+              message.data.color,
+              message.data.x,
+              message.data.y
+            );
+            break;
+          case clickPositionMessage:
+            clickCell(message.data.x, message.data.y);
+            break;
+          case changeColorPaletteMessage:
+            handleChangeColorPalette(
+              message.data.paletteId,
+              message.data.colors
+            );
+            break;
+          case resetMessage:
+            resetGrid();
+            break;
+          case joinedMessage:
+            addUser(message.data.clientId, message.data.color);
+            break;
+          default:
+            break;
+        }
+      }
+    };
+    webSocket.onerror = (event) => {
+      this.error = `WebSocket error ${event.message}`;
+    };
+  } else {
+    webSocket.close();
+    disconnectUser();
+  }
+
+*)
