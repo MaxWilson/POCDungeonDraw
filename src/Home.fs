98ef1da7c49c1e0a48d1cac8398f9b9b1136b398
Max Wilson <wilson.max@gmail.com>
2023-02-10 05:55:14 -0800

    Refactor

--- src/Home.fs
+++ src/Home.fs
@@ -1,4 +1,5 @@
-module App
+﻿module Home
+
 
 open Browser.Dom
 
@@ -234,8 +235,6 @@ let view (model:Model) dispatch =
         Message model.alias
         ]
 
-open Browser.Dom
-open Elmish
 open Elmish.React
 
 module Auth =
@@ -249,28 +248,3 @@ module Auth =
     type AuthResponse = {
         clientPrincipal: Principal option
         }
-
-Program.mkProgram init update view
-    |> Program.withSubscription(fun model ->
-        Cmd.ofSub(fun dispatch ->
-            promise {
-                dispatch (ReceiveIdentity InProgress)
-                let! resp = Thoth.Fetch.Fetch.tryFetchAs ".auth/me"
-                match resp with
-                | Ok (v: Auth.AuthResponse) ->
-                    v.clientPrincipal |> Option.map (function
-                    | v when v.identityProvider = "facebook" -> Facebook, v.userDetails
-                    | v when v.identityProvider = "aad" -> AAD, v.userDetails
-                    | v -> Erroneous, $"{v.identityProvider}/{v.userDetails}")
-                    |> Ready |> ReceiveIdentity
-                | Error err -> ReceiveIdentity (Ready (Some (Erroneous, err.ToString())))
-                |> dispatch
-                if model.pubsubConnection.IsNone then
-                    do! joinChannel "default" dispatch
-            }
-            |> Promise.start
-            )
-        )
-    |> Program.toNavigable Nav.parse Nav.nav
-    |> Program.withReactBatched "root"
-    |> Program.run
